87a670a43ee8bc109e7c669a3b840043da84596b
PacksGamingHD <craftyswans@gmail.com>
2016-08-15 23:52:00 -0700

    Formatting - check lombok later

--- src/main/java/me/totalfreedom/totalfreedommod/rank/Rank.java
+++ src/main/java/me/totalfreedom/totalfreedommod/rank/Rank.java
@@ -1,7 +1,7 @@
 package me.totalfreedom.totalfreedommod.rank;
 
 import org.bukkit.*;
-
+// Do we need lombok?
 public enum Rank implements Displayable
 {
     IMPOSTOR("an", "Impostor", Type.PLAYER, "Imp", ChatColor.YELLOW), 
@@ -12,14 +12,14 @@ public enum Rank implements Displayable
     SENIOR_ADMIN("a", "Senior Admin", Type.ADMIN, "SrA", ChatColor.GOLD), 
     TELNET_CONSOLE("the", "Console", Type.ADMIN_CONSOLE, "Console", ChatColor.DARK_GREEN), 
     SENIOR_CONSOLE("the", "Console", Type.ADMIN_CONSOLE, "Console", ChatColor.LIGHT_PURPLE);
-    
+
     private final Type type;
     private final String name;
     private final String determiner;
     private final String tag;
     private final String coloredTag;
     private final ChatColor color;
-    
+
     private Rank(final String determiner, final String name, final Type type, final String abbr, final ChatColor color) {
         this.type = type;
         this.name = name;
@@ -28,39 +28,41 @@ public enum Rank implements Displayable
         this.coloredTag = (abbr.isEmpty() ? "" : (ChatColor.DARK_GRAY + "[" + color + abbr + ChatColor.DARK_GRAY + "]" + color));
         this.color = color;
     }
-    
+
     @Override
     public String getColoredName() {
         return this.color + this.name;
     }
-    
+
     @Override
     public String getColoredLoginMessage() {
         return this.determiner + " " + this.color + ChatColor.ITALIC + this.name;
     }
-    
+
     public boolean isConsole() {
         return this.getType() == Type.ADMIN_CONSOLE;
     }
-    
+
     public int getLevel() {
         return this.ordinal();
     }
-    
+
     public boolean isAtLeast(final Rank rank) {
         return this.getLevel() >= rank.getLevel() && (!this.hasConsoleVariant() || !rank.hasConsoleVariant() || this.getConsoleVariant().getLevel() >= rank.getConsoleVariant().getLevel());
     }
-    
+
     public boolean isAdmin() {
         return this.getType() == Type.ADMIN || this.getType() == Type.ADMIN_CONSOLE;
     }
-    
+
     public boolean hasConsoleVariant() {
         return this.getConsoleVariant() != null;
     }
-    
-    public Rank getConsoleVariant() {
-        switch (this) {
+
+    public Rank getConsoleVariant()
+    {
+        switch (this)
+        {
             case TELNET_ADMIN:
             case TELNET_CONSOLE: {
                 return Rank.TELNET_CONSOLE;
@@ -74,7 +76,7 @@ public enum Rank implements Displayable
             }
         }
     }
-    
+
     public Rank getPlayerVariant() {
         switch (this) {
             case TELNET_ADMIN:
@@ -90,7 +92,7 @@ public enum Rank implements Displayable
             }
         }
     }
-    
+
     public static Rank findRank(final String string) {
         try {
             return valueOf(string.toUpperCase());
@@ -99,37 +101,37 @@ public enum Rank implements Displayable
             return Rank.NON_OP;
         }
     }
-    
+
     public Type getType() {
         return this.type;
     }
-    
+
     @Override
     public String getName() {
         return this.name;
     }
-    
+
     @Override
     public String getTag() {
         return this.tag;
     }
-    
+
     @Override
     public String getColoredTag() {
         return this.coloredTag;
     }
-    
+
     @Override
     public ChatColor getColor() {
         return this.color;
     }
-    
+
     public enum Type
     {
         PLAYER, 
         ADMIN, 
         ADMIN_CONSOLE;
-        
+
         public boolean isAdmin() {
             return this != Type.PLAYER;
         }
