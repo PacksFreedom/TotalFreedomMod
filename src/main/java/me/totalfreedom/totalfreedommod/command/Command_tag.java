f76982cfc3781bb3ab5f9b9a0b6df382b8a06941
PacksGamingHD <craftyswans@gmail.com>
2016-08-15 23:46:08 -0700

    Formatting

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_tag.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_tag.java
@@ -17,10 +17,13 @@ public class Command_tag extends FreedomCommand
     public static final List<String> FORBIDDEN_WORDS;
     
     public boolean run(final CommandSender sender, final Player playerSender, final Command cmd, final String commandLabel, final String[] args, final boolean senderIsConsole) {
-        if (args.length == 1) {
-            if ("list".equalsIgnoreCase(args[0])) {
+        if (args.length == 1)
+        {
+            if ("list".equalsIgnoreCase(args[0]))
+            {
                 this.msg("Tags for all online players:");
-                for (final Player player : this.server.getOnlinePlayers()) {
+                for (final Player player : this.server.getOnlinePlayers())
+                {
                     final FPlayer playerdata = ((TotalFreedomMod)this.plugin).pl.getPlayer(player);
                     if (playerdata.getTag() != null) {
                         this.msg(player.getName() + ": " + playerdata.getTag());
@@ -28,7 +31,8 @@ public class Command_tag extends FreedomCommand
                 }
                 return true;
             }
-            if ("clearall".equalsIgnoreCase(args[0])) {
+            if ("clearall".equalsIgnoreCase(args[0]))
+            {
                 if (!((TotalFreedomMod)this.plugin).al.isAdmin(sender)) {
                     this.noPerms();
                     return true;
@@ -37,20 +41,21 @@ public class Command_tag extends FreedomCommand
                 int count = 0;
                 for (final Player player2 : this.server.getOnlinePlayers()) {
                     final FPlayer playerdata2 = ((TotalFreedomMod)this.plugin).pl.getPlayer(player2);
-                    if (playerdata2.getTag() != null) {
+                    if (playerdata2.getTag() != null)
+                    {
                         ++count;
                         playerdata2.setTag(null);
                     }
                 }
                 this.msg(count + " tag(s) removed.");
                 return true;
-            }
-            else {
-                if ("off".equalsIgnoreCase(args[0])) {
-                    if (senderIsConsole) {
+            } else {
+                if ("off".equalsIgnoreCase(args[0]))
+                {
+                    if (senderIsConsole)
+                    {
                         this.msg("\"/tag off\" can't be used from the console. Use \"/tag clear <player>\" or \"/tag clearall\" instead.");
-                    }
-                    else {
+                    } else {
                         ((TotalFreedomMod)this.plugin).pl.getPlayer(playerSender).setTag(null);
                         this.msg("Your tag has been removed.");
                     }
@@ -60,7 +65,8 @@ public class Command_tag extends FreedomCommand
             }
         }
         else {
-            if (args.length < 2) {
+            if (args.length < 2)
+            {
                 return false;
             }
             if ("clear".equalsIgnoreCase(args[0])) {
@@ -69,16 +75,17 @@ public class Command_tag extends FreedomCommand
                     return true;
                 }
                 final Player player3 = this.getPlayer(args[1]);
-                if (player3 == null) {
+                if (player3 == null)
+                {
                     this.msg(FreedomCommand.PLAYER_NOT_FOUND);
                     return true;
                 }
                 ((TotalFreedomMod)this.plugin).pl.getPlayer(player3).setTag(null);
                 this.msg("Removed " + player3.getName() + "'s tag.");
                 return true;
-            }
-            else {
-                if ("set".equalsIgnoreCase(args[0])) {
+            } else {
+                if ("set".equalsIgnoreCase(args[0]))
+                {
                     final String inputTag = StringUtils.join((Object[])args, " ", 1, args.length);
                     final String outputTag = FUtil.colorize(StringUtils.replaceEachRepeatedly(StringUtils.strip(inputTag), new String[] { "§", "&k" }, new String[] { "", "" })) + ChatColor.RESET;
                     if (!((TotalFreedomMod)this.plugin).al.isAdmin(sender)) {
@@ -87,7 +94,8 @@ public class Command_tag extends FreedomCommand
                             this.msg("That tag is too long (Max is 20 characters).");
                             return true;
                         }
-                        for (final String word : Command_tag.FORBIDDEN_WORDS) {
+                        for (final String word : Command_tag.FORBIDDEN_WORDS)
+                        {
                             if (rawTag.contains(word)) {
                                 this.msg("That tag contains a forbidden word.");
                                 return true;
@@ -104,6 +112,6 @@ public class Command_tag extends FreedomCommand
     }
     
     static {
-        FORBIDDEN_WORDS = Arrays.asList("admin", "owner", "moderator", "developer", "console","MOD", "CONSOLE", "SA", "STA", "SrA");
+        FORBIDDEN_WORDS = Arrays.asList("admin", "owner", "moderator", "developer", "console","mod", "sa", "sta", "sra");
     }
 }
