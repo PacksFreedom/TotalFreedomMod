9f8ee7df06093db5d3764c40a77e45d2b6dfbc68
PacksGamingHD <craftyswans@gmail.com>
2016-08-15 23:36:26 -0700

    Update BlockBlocker.java

--- src/main/java/me/totalfreedom/totalfreedommod/blocking/BlockBlocker.java
+++ src/main/java/me/totalfreedom/totalfreedommod/blocking/BlockBlocker.java
@@ -11,22 +11,28 @@ import org.bukkit.event.*;
 
 public class BlockBlocker extends FreedomService
 {
-    public BlockBlocker(final TotalFreedomMod plugin) {
+    public BlockBlocker(final TotalFreedomMod plugin)
+    {
         super(plugin);
     }
     
-    protected void onStart() {
+    protected void onStart()
+    {
     }
     
-    protected void onStop() {
+    protected void onStop()
+    {
     }
     
     @EventHandler(priority = EventPriority.HIGH)
-    public void onBlockPlace(final BlockPlaceEvent event) {
+    public void onBlockPlace(final BlockPlaceEvent event)
+    {
         final Player player = event.getPlayer();
-        switch (event.getBlockPlaced().getType()) {
+        switch (event.getBlockPlaced().getType())
+        {
             case LAVA:
-            case STATIONARY_LAVA: {
+            case STATIONARY_LAVA:
+                {
                 if (ConfigEntry.ALLOW_LAVA_PLACE.getBoolean()) {
                     FLog.info(String.format("%s placed lava @ %s", player.getName(), FUtil.formatLocation(event.getBlock().getLocation())));
                     player.getInventory().clear(player.getInventory().getHeldItemSlot());
@@ -38,7 +44,8 @@ public class BlockBlocker extends FreedomService
                 break;
             }
             case WATER:
-            case STATIONARY_WATER: {
+            case STATIONARY_WATER:
+                {
                 if (ConfigEntry.ALLOW_WATER_PLACE.getBoolean()) {
                     FLog.info(String.format("%s placed water @ %s", player.getName(), FUtil.formatLocation(event.getBlock().getLocation())));
                     player.getInventory().clear(player.getInventory().getHeldItemSlot());
@@ -49,7 +56,8 @@ public class BlockBlocker extends FreedomService
                 event.setCancelled(true);
                 break;
             }
-            case FIRE: {
+            case FIRE:
+                {
                 if (ConfigEntry.ALLOW_FIRE_PLACE.getBoolean()) {
                     FLog.info(String.format("%s placed fire @ %s", player.getName(), FUtil.formatLocation(event.getBlock().getLocation())));
                     player.getInventory().clear(player.getInventory().getHeldItemSlot());
@@ -60,7 +68,8 @@ public class BlockBlocker extends FreedomService
                 event.setCancelled(true);
                 break;
             }
-            case TNT: {
+            case TNT:
+                {
                 if (ConfigEntry.ALLOW_EXPLOSIONS.getBoolean()) {
                     FLog.info(String.format("%s placed TNT @ %s", player.getName(), FUtil.formatLocation(event.getBlock().getLocation())));
                     player.getInventory().clear(player.getInventory().getHeldItemSlot());
@@ -71,12 +80,14 @@ public class BlockBlocker extends FreedomService
                 event.setCancelled(true);
                 break;
             }
-            case STRUCTURE_BLOCK:{
+            case STRUCTURE_BLOCK:
+                {
                 player.sendMessage(ChatColor.GRAY + "Use of structure blocks are disabled.");
                 event.setCancelled(true);
                 break;
             }
-            case WRITTEN_BOOK:{
+            case WRITTEN_BOOK:
+                {
                 player.sendMessage(ChatColor.GRAY + "Use of Written Book are disabled.");
                 event.setCancelled(true);
                 break;
